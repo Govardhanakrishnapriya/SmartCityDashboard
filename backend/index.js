678cbe77494253001d3983499a8dfdcd5cff2bb6
Govardhanakrishnapriya <govardhanakrishnapriya04@gmail.com>
2026-04-18 16:13:32 +0530

    fe url

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin:"https://smart-city-dashboard-l6j3.vercel.app",
     methods: ["GET", "POST"]
   }
 });
